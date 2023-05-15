c10e26a5696467e5e7427d83a36eae1471d206cf
fchatys <fchatys@gmail.com>
2023-05-15 20:07:50 +0200

    Aktualizacja notatek

---
+++ src/Brudnopisy/Brud.java
@@ -0,0 +1,17 @@
+package Brudnopisy;
+
+public class Brud {
+
+    public static void main(String[] args) {
+
+        int num;
+
+        num = 9;
+
+        System.out.println(num);
+
+    }
+
+
+
+}
